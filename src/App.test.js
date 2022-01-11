c9526bc13afdfe173393e22b4f2daf9ebe937e59
Gustavo Velasquez <gvelasquez@consultoriaglobal.com.ar>
2022-01-11 00:15:14 -0300

    init project

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hola Mundo!!/i);
+  console.log(linkElement)
   expect(linkElement).toBeInTheDocument();
 });
